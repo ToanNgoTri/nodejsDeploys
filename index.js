9cfbc65d5bf27c73ed09fb0c8d00c1c9e926ae66
ToanNgoTri <ngotritoan33@gmail.com>
2024-02-29 09:58:00 +0700

    c

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const data = require('./data')
+var cors = require('cors')
+app.use(cors())
 
 app.listen( process.env.PORT || 3000, () => {
     console.log("App listening on port 3000");
